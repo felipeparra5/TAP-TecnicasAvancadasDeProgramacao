f0475cb40776a1b3e756c250872e3260a133b310
Felipe Parra Alves <101674505+felipeparra5@users.noreply.github.com>
2023-09-27 17:31:02 -0300

    Arquivos em JAVA
    Exercício feito pela aula de TAP, porém em arquivo JAVA.

---
+++ Aula 1 - Testes com JAVA/Exemplo1.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author Felipe Parra Alves
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println ("Ola Mundo"); 
+        System.out.println("UmDoisTres");
+        System.out.println("Um\nDois\nTres");
+        System.out.println("BolaLápis");
+        System.out.println("Bola\tLápis");
+    }
+    
+}
---
+++ Aula 1 - Testes com JAVA/Exemplo2.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Felipe Parra Alves
+ */
+public class Exemplo2 {
+    public static void main(String[] args) {
+        int idade;
+        double altura; 
+        char sexo;
+        String nome;
+        
+        idade=27;
+        altura=1.75;
+        sexo='M';
+        nome= "Pedro Silveira";
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("sSexo: " + sexo);
+        System.out.println("Altura: " + altura);
+    }
+    
+}
